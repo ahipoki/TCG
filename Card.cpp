d96a4b84f728a0672d94e98b258e0aacdc598e3e
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-03-11 08:27:02 -0700

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include "Card.h"
+
+using namespace std;
+
+Card::Card(char* newName){
+  name = new char[strlen(newName}+1];
+  strcpy(name, newName);
+}
+
+char* Card::getName(){
+  return name;
+}
